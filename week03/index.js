ccf2bcbe91435b82abc93bca929caebcca95fcfd
Jarett Wallace <Jwallace02@rio.edu>
2026-01-28 15:20:40 -0500

    "Initialize Week 3 Node project and runtime config

--- week03/index.js
+++ week03/index.js
@@ -1,16 +1,22 @@
-// Identification Header
-const name = "Jarett Wallace";
-const course = "CS31103";
-const week = "week 3";
-// Runtime info
-const nodeVersion = process.version
-const currentDate = new Date();
-// Config via envirnment variables
-const port = process.env.Port || 3000;
-const environment = process.env.NODE_ENV || 'development';
-// Data OBJ
+// ID header 
+console.log("Jarett Wallace");
+console.log("course: CS31103");
+console.log("week 03");
+
+// runtime info
+console.log("node version: ", process.version);
+console.log("Current date and time:", new Date());
+
+// config via environment variables
+const port = process.env.PORT|| 3000;
+const environment = process.env.NODE_ENV || "development";
+
+// data obj
 const appConfig = {
-    port,
-    environment,
-    startedAT: currentDate
+  port,
+  environment,
+  startedAt: new Date()
 };
+// print as JSON
+console.log(" App Configuration:");
+console.log(JSON.stringify(appConfig, null, 2));
---
+++ week03/readme.md
@@ -0,0 +1,7 @@
+* package.json is used as a way to store information for applications, modules, and packages. Without json any applications made will not function properlly.
+
+* Process.env is a global object in node.js that both stores variables and provides access to them at the same time.
+
+* npm start is a command in a package.json file that is normally used to start an application. When it is ran it will look for a "start" script in the json file and will execute the command that is tied to the start
+
+* I kept running into an error when attempting to run npm start that kept saying "node version: ProcessInstruction.version);. I threw this eroor along with my code into Chatgpt to try and help figure out what was causing it, as it turns out autopilot which i cant fiigure out how to turn off in VS kept changing process into Processing and that is what kept causing me to have errors in my code 
