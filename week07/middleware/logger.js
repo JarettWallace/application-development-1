2c7a34c218a885553a5af1cb2a1576ad067f0887
Jarett Wallace <Jwallace02@rio.edu>
2026-02-28 23:36:51 -0500

    updated logger.js to work with the timer

--- week07/middleware/logger.js
+++ week07/middleware/logger.js
@@ -1,8 +1,18 @@
 // middleware/logger.js
 
 const logger = (req, res, next) => {
+  const start = Date.now();
   const timestamp = new Date().toISOString();
-  console.log(`[${timestamp}] ${req.method} ${req.originalUrl}`);
+
+  console.log(`[${timestamp}] ${req.method} ${req.originalUrl} - started`);
+
+  res.on("finish", () => {
+    const duration = Date.now() - start;
+    console.log(
+      `[${req.method} ${req.originalUrl}] completed in ${duration}ms`
+    );
+  });
+
   next();
 };
 
