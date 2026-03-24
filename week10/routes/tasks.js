7ceb8f5e65c265263c8f03e52bddf99c14475ff3
Jarett Wallace <Jwallace02@rio.edu>
2026-03-24 15:56:45 -0400

    added in a route for tasks

---
+++ week10/routes/tasks.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  getAllTasks,
+  getTaskById,
+  createTask,
+  updateTask,
+  deleteTask
+} = require("../controllers/tasksController");
+
+// Routes
+router.get("/", getAllTasks);
+router.get("/:id", getTaskById);
+router.post("/", createTask);
+router.patch("/:id", updateTask);
+router.delete("/:id", deleteTask);
+
+module.exports = router;
